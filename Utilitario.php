02e734e99ef2d821e290804516b357efe71c39db
Vinícius Moura <30481009+viniciusmouraesilva@users.noreply.github.com>
2018-01-05 21:09:34 -0200

    Novo utilitario
    Novo utilitario e documentação.

--- Utilitario.php
+++ Utilitario.php
@@ -2,11 +2,13 @@
 
 class verificarDados {
 
+	//nome dos dados em post separados por |. Exemplo: dado1|dado2
+	
 	public function verificarPostEGetPHP7($nomes_post) {	
 
 		if(strstr($nomes_post,'|')) {
 
-			$array = explode('|',$nomes_post);
+			$array = explode('|', $nomes_post);
 
 			$total_enviado = count($array);
 
@@ -14,9 +16,9 @@ class verificarDados {
 
 			$dados = [];
 
-			//operador null colesce
 			while($i < $total_enviado) {
 
+				//operador null colesce
 				$dados["{$array[$i]}"] = $_POST["{$array[$i]}"] ?? $_GET["{$array[$i]}"] ?? '';
 
 				$i++;
@@ -28,3 +30,24 @@ class verificarDados {
 			return '';
 		}
 	}
+
+	public function limparString($dado) {
+
+		try {
+
+			if(is_string($dado)) {
+
+				$filtrado = filter_var($dado,FILTER_SANITIZE_STRING);
+
+				return $filtrado;
+
+			}else {
+				throw new Exception();
+			}
+
+		}catch(Exception $ex) {
+			return '';
+		}
+	}
+
+}
